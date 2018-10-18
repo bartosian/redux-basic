12b4e8a336b8e5c5034d86f81324b8db6872f3aa
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-18 14:36:45 -0400

    Done todoreducer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ const TODO_ADD = 'TODO_ADD';
 const TODO_TOGGLE = 'TODO_TOGGLE';
 const FILTER_SET = 'FILTER_SET';
 
+const todos = [
+    {id:0, name: "Learn redux"},
+    {id:1, name: "Buy new bag"}
+];
+
+function todoReducer(state=todos, action) {
+    switch (action.type) {
+        case TODO_ADD: {
+            return applyAddToDO(state, action);
+        }
+
+        case TODO_TOGGLE : {
+        return applyToggleTodo(state, action);
+        }
+        default : return state;
+    }
+}
+
 
 function TodoApp() {
     return <div>Todo App</div>;
